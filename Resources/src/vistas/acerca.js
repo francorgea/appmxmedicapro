c1ce6af6c43c65cfae89dd7ae06e81938e2191b5
Juan Carlos Salinas Ojeda <juan.carlos@jasoluciones.com.mx>
2020-09-22 23:26:34 +0000

    modificaciones menores

--- Resources/src/vistas/acerca.js
+++ Resources/src/vistas/acerca.js
@@ -3,18 +3,16 @@
 //
 //
 function Mostrar(win){
-  
+
     var Button = require("src/common/Button");
     var Input = require("src/common/InputNS");
     win.backgroundImage =  "/images/fondos/fondo.png";
-    
-    
-    
+
     // Definimos el contenedor y el scroll
-    var scroll = Ti.UI.createScrollView({ 
+    var scroll = Ti.UI.createScrollView({
         top: '10%',
         width: Ti.UI.FILL,
-        height: Ti.UI.FILL,  
+        height: Ti.UI.FILL,
     });
     var contenedor = Ti.UI.createView({
         // layout: 'vertical',
@@ -23,7 +21,7 @@ function Mostrar(win){
     });
     scroll.add(contenedor);
     win.add(scroll);
-    
+
 		var logo = Ti.UI.createImageView({
 	  			image: 'images/logo.png',
 	  			height: '15%',
@@ -31,51 +29,50 @@ function Mostrar(win){
 	  			top: '50%',
 	  			zIndex: 9999
   });
-  
   contenedor.add(logo);
 
-    
-    var nombre_app = Ti.UI.createLabel({ 
-	    			top: '70%',
-        text: params.app_name, 
-        font: { fontFamily: params.fuente_primaria, fontSize: 16  } , 
+
+    var nombre_app = Ti.UI.createLabel({
+	    	top: '70%',
+        text: params.app_name,
+        font: { fontFamily: params.fuente_primaria, fontSize: 16  } ,
         color: params.color0,
     });
     contenedor.add(nombre_app);
-    
-    
-    // Espacio
-    contenedor.add(Ti.UI.createView({ height: 10 })); 
 
-    var version = Ti.UI.createLabel({ 
+
+    // Espacio
+    contenedor.add(Ti.UI.createView({ height: 10 }));
+
+    var version = Ti.UI.createLabel({
 	    			top: '75%',
-        text: "Versión " + params.app_version, 
-        font: { fontFamily: params.fuente_primaria, fontSize: 16  } , 
+        text: "Versión " + params.app_version,
+        font: { fontFamily: params.fuente_primaria, fontSize: 16  } ,
         color: params.color0,
     });
     contenedor.add(version);
-    
+
     // Espacio
-    contenedor.add(Ti.UI.createView({ height: 40 }));  
-            
-    
-    var gea = Ti.UI.createLabel({ 
+    contenedor.add(Ti.UI.createView({ height: 40 }));
+
+
+    var gea = Ti.UI.createLabel({
 	    			top: '82%',
-        text: "GEA Internacional SA de CV", 
+        text: "GEA Internacional SA de CV",
         color: params.color7,
-        font: { fontFamily: params.fuente_primaria, fontSize: 16  } , 
+        font: { fontFamily: params.fuente_primaria, fontSize: 16  } ,
     });
     contenedor.add(gea);
-    var derechos = Ti.UI.createLabel({ 
+    var derechos = Ti.UI.createLabel({
         top: '87%',
         text: "(c) 2018 Todos los derechos reservados",
-        color: params.color7, 
-        font: { fontFamily: params.fuente_primaria, fontSize: 16  } , 
-        
+        color: params.color7,
+        font: { fontFamily: params.fuente_primaria, fontSize: 16  } ,
+
     });
     contenedor.add(derechos);
-    
-    contenedor.add(Ti.UI.createView({ height:40 }));  
-    
+
+    contenedor.add(Ti.UI.createView({ height:40 }));
+
 }
-exports.Mostrar = Mostrar;  
+exports.Mostrar = Mostrar;
